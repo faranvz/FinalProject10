cac82644f5cfc40a6cbc4ccbbd67fa1c4004888a
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2025-10-06 13:58:56 +0700

    menambahkan baris ketiga

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Hallo0, ini baris kedua oleh Adel");
+        System.out.println("Hallo, ini baris ketiga oleh dewi!");
     }
 }
