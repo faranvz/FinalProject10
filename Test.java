fca4934f324c0d1eb60d16cfa4f5f0ed9157fb6f
faranvz <s4tgo77@gmail.com>
2025-10-06 14:02:33 +0700

    Menambahkan baris keempat oleh Fara

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Hallo0, ini baris kedua oleh Adel");
         System.out.println("Hallo, ini baris ketiga oleh dewi!");
+        System.out.println("Halo, ini baris keempat oleh Fara");
     }
 }
